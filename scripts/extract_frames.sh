5d3cd0960f7f8e93296e23072ca4a14e2f0a45c5
joseprs <josep.reverter99@gmail.com>
2022-02-19 23:12:32 +0100

    extract frames new update

--- scripts/extract_frames.sh
+++ scripts/extract_frames.sh
@@ -8,23 +8,23 @@ scale="1.0"
 iext="jpg"
 
 video_ini="$dname"/video.ini
-if [ -f "$video_ini" ]; then    
-  start_sec=`grep -A2 '\['"$fname"'\]' "$video_ini" | tail -2 | head -1 | sed 's/.*= *//g'`    
+if [ -f "$video_ini" ]; then
+  start_sec=`grep -A2 '\['"$fname"'\]' "$video_ini" | tail -2 | head -1 | sed 's/.*= *//g'`
   start_time=`python3 -c 'import datetime; print(str(datetime.timedelta(seconds = '"$start_sec"')))'`
 
   duration_sec=`grep -A2 '\['"$fname"'\]' "$video_ini" | tail -1 | sed 's/.*= *//g'`
-  duration_time=`python3 -c 'import datetime; print(str(datetime.timedelta(seconds = '"$duration_sec"')))'` 
+  duration_time=`python3 -c 'import datetime; print(str(datetime.timedelta(seconds = '"$duration_sec"')))'`
 else
   start_time="00:00:00.0"
 
   duration=`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$1"`
-  duration_time=`python3 -c 'import datetime; print(str(datetime.timedelta(seconds = '"$duration"')))'`    
+  duration_time=`python3 -c 'import datetime; print(str(datetime.timedelta(seconds = '"$duration"')))'`
 fi
 
 scale_h="$scale"
 sar=`ffprobe -v error -select_streams v:0 -show_entries stream=sample_aspect_ratio -of default=noprint_wrappers=1:nokey=1 "$1"`
 if [ $sar != "N/A" ]; then
-  sar=`echo "$sar" | tr ':' '/'`  
+  sar=`echo "$sar" | tr ':' '/'`
   scale_w=`python3 -c 'sar='"$sar"'; print('"$scale"' if sar <= 1 else '"$scale"'*sar)'`
 else
   scale_w="$scale"
